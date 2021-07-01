fb8724a3ea6d58dad66772ee1d4ee4c8e12e5606
mafei <mafei.dev@gmail.com>
2021-07-01 18:02:57 +0530

    flux with range [seems like for loop]

---
+++ src/main/java/com/mafei/section2/L05FluxRange.java
@@ -0,0 +1,26 @@
+package com.mafei.section2;
+
+import com.mafei.utils.SubscriberUtil;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+  @Author mafei
+*/
+public class L05FluxRange {
+    public static void main(String[] args) {
+
+        //just like a for loop
+        Flux.range(1, 20)
+                .map(integer -> SubscriberUtil.FAKER.name().firstName())
+                .subscribe(
+                        SubscriberUtil.onNext(),
+                        SubscriberUtil.onError(),
+                        SubscriberUtil.onComplete()
+                );
+
+
+    }
+}
