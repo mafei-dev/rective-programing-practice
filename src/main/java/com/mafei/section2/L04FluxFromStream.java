230fade579db53b8d0f7caac818a447ea9fedc31
mafei <mafei.dev@gmail.com>
2021-07-01 17:53:21 +0530

    problem with flux from stream

--- src/main/java/com/mafei/section2/L04FluxFromStream.java
+++ src/main/java/com/mafei/section2/L04FluxFromStream.java
@@ -15,9 +15,27 @@ public class L04FluxFromStream {
 
         List<Integer> integerList = List.of(1, 2, 3, 4);
         Stream<Integer> integerStream = integerList.stream();
-        integerStream.forEach(System.out::println); //stream is closed after use one time
+        //integerStream.forEach(System.out::println); //stream is closed after use one time
         //stream can't be use multiple times
         //integerStream.forEach(System.out::println);
+
+        Flux<Integer> integerFlux = Flux.fromStream(integerStream);
+
+        //subscriber 1
+        //use the stream successfully
+        integerFlux.subscribe(
+                SubscriberUtil.onNext(),
+                SubscriberUtil.onError(),
+                SubscriberUtil.onComplete()
+        );
+
+        //subscriber 2
+        //an error will throw due to the 2nd use [stream has already been operated upon or closed]
+        integerFlux.subscribe(
+                SubscriberUtil.onNext(),
+                SubscriberUtil.onError(),
+                SubscriberUtil.onComplete()
+        );
 
 
     }
