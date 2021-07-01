6d116a7153b8f0c793eabd619e2e5c8618b9a054
mafei <mafei.dev@gmail.com>
2021-07-01 15:45:02 +0530

    mono from callable

--- src/main/java/com/mafei/section1/L05MonoFormSupplier.java
+++ src/main/java/com/mafei/section1/L05MonoFormSupplier.java
@@ -3,6 +3,8 @@ package com.mafei.section1;
 import com.mafei.utils.SubscriberUtil;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.Callable;
+
 /*
   @Author mafei
 */
@@ -17,6 +19,9 @@ public class L05MonoFormSupplier {
         //if you haven't data already, you can use this method to create a mono without any execution
         //if you want to do only when subscribed,use this method.
         Mono.fromSupplier(() -> getName());
+
+        Callable<String> callable = () -> getName();
+        Mono.fromCallable(callable).subscribe(SubscriberUtil.onNext());
 
 
     }
