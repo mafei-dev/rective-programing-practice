8e65717d8d22ea29f4e017c0f64ee32fe1f6321c
mafei <mafei.dev@gmail.com>
2021-07-01 17:48:57 +0530

    stream usage intro

---
+++ src/main/java/com/mafei/section2/L04FluxFromStream.java
@@ -0,0 +1,24 @@
+package com.mafei.section2;
+
+import com.mafei.utils.SubscriberUtil;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+  @Author mafei
+*/
+public class L04FluxFromStream {
+    public static void main(String[] args) {
+
+        List<Integer> integerList = List.of(1, 2, 3, 4);
+        Stream<Integer> integerStream = integerList.stream();
+        integerStream.forEach(System.out::println); //stream is closed after use one time
+        //stream can't be use multiple times
+        //integerStream.forEach(System.out::println);
+
+
+    }
+}
