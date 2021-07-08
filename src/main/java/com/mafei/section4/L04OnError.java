971b3adcccdd71e7677771fbc144599203c1e5a2
mafei <mafei.dev@gmail.com>
2021-07-08 14:21:39 +0530

    Flux OnError callback function

--- src/main/java/com/mafei/section4/L04OnError.java
+++ src/main/java/com/mafei/section4/L04OnError.java
@@ -2,6 +2,7 @@ package com.mafei.section4;
 
 import com.mafei.utils.SubscriberUtil;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -13,10 +14,14 @@ public class L04OnError {
         System.out.println("Main Thread > " + Thread.currentThread().getName());
         Flux.range(1, 10)
                 .map(integer -> 10 / (5 - integer))
-                .onErrorReturn(-1)
+                .onErrorResume(throwable -> fallback())
                 .subscribe(integer -> {
                     System.out.println("Thread > " + Thread.currentThread().getName());
                     System.out.println("integer = " + integer);
                 });
+    }
+
+    public static Mono<Integer> fallback() {
+        return Mono.fromSupplier(() -> SubscriberUtil.FAKER.random().nextInt(100, 200));
     }
 }
