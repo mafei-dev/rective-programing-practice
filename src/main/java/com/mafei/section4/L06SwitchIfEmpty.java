e15dfac0ac1172531d88ac1375029f17d55f4406
mafei <mafei.dev@gmail.com>
2021-07-08 19:40:29 +0530

    Flux with switchIfEmpty

---
+++ src/main/java/com/mafei/section4/L06SwitchIfEmpty.java
@@ -0,0 +1,49 @@
+package com.mafei.section4;
+
+import reactor.core.publisher.Flux;
+
+/*
+  @Author mafei
+*/
+public class L06SwitchIfEmpty {
+    public static void main(String[] args) {
+
+        //01
+        getOrderNums()
+                .filter(integer -> integer > 10)
+                .switchIfEmpty(s -> {
+                    System.out.println("switchIfEmpty fallback");
+                })
+                .subscribe(integer -> {
+                    System.out.println("1-integer = " + integer);
+                });
+        //02
+        getOrderNums()
+                .filter(integer -> integer > 50)
+                .switchIfEmpty(fallback())
+                .filter(integer -> integer > 12)
+                .subscribe(integer -> {
+                    System.out.println("1-integer = " + integer);
+                });
+
+        //03
+        getOrderNums()
+                .filter(integer -> integer > 10)
+                .switchIfEmpty(fallback())
+                .subscribe(integer -> {
+                    System.out.println("1-integer = " + integer);
+                });
+
+
+    }
+
+    public static Flux<Integer> getOrderNums() {
+        return Flux.range(1, 10);
+    }
+
+    public static Flux<Integer> fallback() {
+        return Flux.range(20, 10);
+    }
+
+
+}
