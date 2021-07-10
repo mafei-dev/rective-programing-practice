18b5966ed0670471a9b5c214d8b525da6ea60c26
mafei <mafei.dev@gmail.com>
2021-07-10 22:25:49 +0530

    publish ref count

---
+++ src/main/java/com/mafei/section5/L03HotPublishRefCount.java
@@ -0,0 +1,48 @@
+package com.mafei.section5;
+
+import com.mafei.utils.SubscriberUtil;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+/*
+  @Author mafei
+*/
+public class L03HotPublishRefCount {
+    public static void main(String[] args) {
+
+        //share = publish().refCount(1)
+        Flux<String> movieStream = Flux.fromStream(() -> getMovies())
+                .delayElements(Duration.ofSeconds(2))
+                //share = publish().refCount(1)
+                //this means, at least should have one subscriber
+                .publish().refCount(1);
+
+
+        movieStream.subscribe(s -> {
+            System.out.println("user 1 : " + s);
+        });
+
+        SubscriberUtil.seep(5);
+
+        movieStream.subscribe(s -> {
+            System.out.println("user 2 : " + s);
+        });
+
+        SubscriberUtil.seep(60);
+
+    }
+
+    public static Stream<String> getMovies() {
+        System.out.println("L01ColdPublisher.getMovies");
+        return Stream.of(
+                "Scene 1",
+                "Scene 2",
+                "Scene 3",
+                "Scene 4",
+                "Scene 5",
+                "Scene 7"
+        );
+    }
+}
